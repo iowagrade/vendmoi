353fe480cfac657b1bb592e752da5f60d45463d1
Brett <brett@iowagrade.com>
2019-05-06 04:01:34 -0800

    After adding side menu, before add map
    Signed-off-by: Brett <brett@iowagrade.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,34 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  public appPages = [
+    {
+      title: 'Login',
+      url: '/login',
+      icon: 'contact'
+    },
+    {
+      title: 'Home',
+      url: '',
+      icon: 'home'
+    },
+    {
+      title: 'Rx',
+      url: '/tabs/tab2',
+      icon: 'medkit'
+    },
+    {
+      title: 'Cart',
+      url: '/shopping-cart',
+      icon: 'cart'
+    },
+    {
+      title: 'Checkout',
+      url: '/check-out',
+      icon: 'done-all'
+    }
+  ];
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
